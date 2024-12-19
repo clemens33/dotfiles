792607aadbe26bebaa8097e89a1b9d49ff5494ce
clemens33 <clemens.kr@gmail.com>
2024-12-19 12:09:13 +0100

    fix for fish functions

--- fish/functions/kcet.fish
+++ fish/functions/kcet.fish
@@ -1,3 +1,3 @@
 function kcet --description "kubectl for da-k8s-emea-test"
-    kubectl --kubeconfig ~/.kube/da-k8s-emea-test --context AKS-DEW-Analytics-001-pinniped $argv
+    kubectl --kubeconfig ~/.kube/da-k8s-emea-test --context AKS-DEW-Analytics-01-pinniped $argv
 end
