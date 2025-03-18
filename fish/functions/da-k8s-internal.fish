11623dfe0030f2ac64793df1bd7b86bf95c25bf9
clemens33 <clemens.kr@gmail.com>
2025-03-18 13:15:25 +0100

    typo fix

--- fish/functions/da-k8s-internal.fish
+++ fish/functions/da-k8s-internal.fish
@@ -1,5 +1,5 @@
 function da-k8s-internal
-    set -gx KUBECONFIG=~/.kube/da.miccust.dev
+    set -gx KUBECONFIG ~/.kube/da.miccust.dev
     kubectl config use-context MIC-DA-pinniped
     kubectl config current-context
     kubectl cluster-info
